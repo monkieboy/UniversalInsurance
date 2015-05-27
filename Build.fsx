3f303ec80eb5b33e74e9ea4fd1a622b429193549
info@millbank <in@magsolutionslimited.co.uk>
2015-05-27 19:27:50 +0100

    Adding the test project so that the testing can start to drive out the requirements.

--- Build.fsx
+++ Build.fsx
@@ -4,11 +4,33 @@ open Fake
 RestorePackages()
 
 let buildDir = "./build/"
+let testDir = "./test/"
+let testDlls = !!"./test/*Tests.dll"
 
 Target "Clean" (fun _ -> CleanDir buildDir)
-Target "Build" (fun _ -> !! "src/**/*.fsproj" |> MSBuildRelease buildDir "Build" |> Log "BuildOutput: ")
+Target "Build" (fun _ -> 
+    !! "src/**/*.csproj" 
+    |> MSBuildRelease buildDir "Build" 
+    |> Log "BuildOutput: ")
+
+Target "BuildTests" (fun _ -> 
+    !! "src/**/*Tests.csproj" 
+    |> MSBuildDebug testDir "Build" |> Log "TestBuild-Output: ")
+
+Target "RunTests" (fun _ -> 
+    testDlls
+      |> NUnit (fun p ->
+          {p with
+             DisableShadowCopy = true;
+             OutputFile = testDir + "TestResults.xml" })
+)
+
+Target "FinishBuild" (fun _ -> trace "Build Script Fininshed")
 
 "Clean"
     ==> "Build"
+    ==> "BuildTests"
+    ==> "RunTests"
+    ==> "FinishBuild"
 
-RunTargetOrDefault "Build"
+RunTargetOrDefault "FinishBuild"
